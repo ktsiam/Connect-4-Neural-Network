3a11e63b249fbb2f75e76d6f7c1ff4d073c72e1b
ktsiam <ktsiampouris@gmail.com>
2017-10-09 14:25:41 -0400

    test

--- Makefile
+++ Makefile
@@ -3,14 +3,14 @@ CXXFLAGS = -std=c++11 -Wall -Wextra -pedantic #-Weverything #-O3
 LDFLAGS  = 
 
 
-play: score4.o main.o 
+play: ./src/objects/score4.o ./src/objects/main.o 
 	${CXX} ${CXXFLAGS} ${LDFLAGS} -o	play	./src/objects/score4.o ./src/objects/main.o
 
 
-score4.o: ./src/score4/score4.cpp
+./src/objects/score4.o: ./src/score4/score4.cpp
 	${CXX} ${CXXFLAGS} -c -o ./src/objects/score4.o ./src/score4/score4.cpp
 
-main.o: ./src/main.cpp
+./src/objects/main.o: ./src/main.cpp
 	${CXX} ${CXXFLAGS} -c -o ./src/objects/main.o ./src/main.cpp
 clean:
 	rm -f play
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 struct Player{
-        int choose(Bitboard me, Bitboard you) { return rand()%7; }
+        int choose(Bitboard me, Bitboard you) { return rand()%8; }
 };
 
 int GAME_OVER = 0;
@@ -10,14 +10,18 @@ int GAME_OVER = 0;
 int main()
 {
         srand(time(NULL));
-        BitScoreFour a;
-        Player p1, p2;
-       
-        while (GAME_OVER == 0) {
-                while (!a.play((File)p1.choose(a.get_pos(WHITE), a.get_pos(BLACK))));
-                if (GAME_OVER != 0)
-                        break;
-                while (!a.play((File)p2.choose(a.get_pos(BLACK), a.get_pos(WHITE))));
+        for (int i = 0; i < 1000; ++i){
+                std::cout << "GAME STARTS\n";
+                GAME_OVER = 0;
+                BitScoreFour a;
+                Player p1, p2;
+                
+                while (GAME_OVER == 0) {
+                        while (!a.play((File)p1.choose(a.get_pos(WHITE), a.get_pos(BLACK))));
+                        if (GAME_OVER != 0)
+                                break;
+                        while (!a.play((File)p2.choose(a.get_pos(BLACK), a.get_pos(WHITE))));
+                }
         }
 
 }
